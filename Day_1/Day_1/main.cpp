b9ea36e6ae0b72ad23e1816a7caf6fd682dd5307
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-12-01 11:00:05 +0000

    Initial commit

---
+++ Day_1/Day_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Day_1
+//
+//  Created by Fiona Stanley on 01/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
