a6dbb6df05e17fe3c7964fe0579ebc9e636ccc16
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-12-04 13:08:16 +0000

    Refactor into functions

--- Day_4/part2.cpp
+++ Day_4/part2.cpp
@@ -10,6 +10,51 @@
 #include <map>
 #include <sstream>
 
+bool isValidBYR(std::string & byr) {
+    if(byr.length() != 4 || std::stoi(byr) < 1920 || std::stoi(byr) > 2002) return false;
+    return true;
+}
+
+bool isValidIYR(std::string iyr) {
+    if(iyr.length() != 4 || std::stoi(iyr) < 2010 || std::stoi(iyr) > 2020) return false;
+    return true;
+}
+
+bool isValidEYR(std::string eyr) {
+    if(eyr.length() != 4 || std::stoi(eyr) < 2020 || std::stoi(eyr) > 2030) return false;
+    return true;
+}
+
+bool isValidHGT(std::string hgt) {
+    if(hgt.find("in") == std::string::npos && hgt.find("cm") == std::string::npos) return false;
+
+    if(hgt.find("cm") != std::string::npos) {
+        int index = hgt.find("cm");
+        std::string height = hgt.substr(0, index);
+        if (stoi(height) < 150 || stoi(height) > 193) return false;
+    } else if (hgt.find("in") != std::string::npos) {
+        int index = hgt.find("in");
+        std::string height = hgt.substr(0, index);
+        if (stoi(height) < 59 || stoi(height) > 76) return false;
+    }
+    return true;
+}
+
+bool isValidHCL(std::string hcl) {
+    if(hcl.length() != 7 || hcl[0] != '#' || hcl.find_first_not_of("#0123456789abcdef") != std::string::npos) return false;
+    return true;
+}
+
+bool isValidECL(std::string ecl) {
+    if(ecl != "amb" && ecl != "blu" && ecl != "brn" && ecl != "gry" && ecl != "grn" && ecl != "hzl" && ecl != "oth") return false;
+    return true;
+}
+
+bool isValidPID(std::string pid) {
+    if(pid.length() != 9 || pid.find_first_not_of("0123456789") != std::string::npos) return false;
+    return true;
+}
+
 bool isValidPassport(std::map<std::string, std::string> & passport) {
     if (passport.count("byr") == 0) return false;
     else if (passport.count("iyr") == 0) return false;
@@ -19,26 +64,13 @@ bool isValidPassport(std::map<std::string, std::string> & passport) {
     else if (passport.count("ecl") == 0) return false;
     else if (passport.count("pid") == 0) return false;
 
-    if(passport["byr"].length() != 4 || std::stoi(passport["byr"]) < 1920 || std::stoi(passport["byr"]) > 2002) return false;
-    if(passport["iyr"].length() != 4 || std::stoi(passport["iyr"]) < 2010 || std::stoi(passport["iyr"]) > 2020) return false;
-    if(passport["eyr"].length() != 4 || std::stoi(passport["eyr"]) < 2020 || std::stoi(passport["eyr"]) > 2030) return false;
-
-    if(passport["hgt"].find("in") == std::string::npos && passport["hgt"].find("cm") == std::string::npos) return false;
-    if(passport["hgt"].find("cm") != std::string::npos) {
-        int index = passport["hgt"].find("cm");
-        std::string height = passport["hgt"].substr(0, index);
-        if (stoi(height) < 150 || stoi(height) > 193) return false;
-    } else if (passport["hgt"].find("in") != std::string::npos) {
-        int index = passport["hgt"].find("in");
-        std::string height = passport["hgt"].substr(0, index);
-        if (stoi(height) < 59 || stoi(height) > 76) return false;
-    }
-
-    if(passport["hcl"].length() != 7 || passport["hcl"][0] != '#' || passport["hcl"].find_first_not_of("#0123456789abcdef") != std::string::npos) return false;
-
-    if(passport["ecl"] != "amb" && passport["ecl"] != "blu" && passport["ecl"] != "brn" && passport["ecl"] != "gry" && passport["ecl"] != "grn" && passport["ecl"] != "hzl" && passport["ecl"] != "oth") return false;
-
-    if(passport["pid"].length() != 9 || passport["pid"].find_first_not_of("0123456789") != std::string::npos) return false;
+    if(!isValidBYR(passport["byr"])) return false;
+    if(!isValidIYR(passport["iyr"])) return false;
+    if(!isValidEYR(passport["eyr"])) return false;
+    if(!isValidHGT(passport["hgt"])) return false;
+    if(!isValidHCL(passport["hcl"])) return false;
+    if(!isValidECL(passport["ecl"])) return false;
+    if(!isValidPID(passport["pid"])) return false;
 
     return true;
 }
